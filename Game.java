086d10c13229582e0b9e5c9c0b9df853cf5a25b9
Jeanna Gindi <jeanna.gindi@jgindi-03.workdayinternal.com>
2016-07-05 20:03:11 -0700

    Game Interface and TicTacToe class

---
+++ Game.java
@@ -0,0 +1,5 @@
+public interface Game{
+    public int move(int x, int y, boolean player);
+    public int isGameOver(int x, int y);
+}
+
---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+public class TicTacToe implements Game{
+    public enum Player{
+        PLAYER1, PLAYER2;
+    }
+    boolean[][] board = new boolean[3][3];
+    @Override
+    public int move(int x, int y, boolean player){
+        return 0;
+    }
+    @Override
+    public int isGameOver(int x, int y){
+        return 0;
+    }
+    public static void main(String[] args){
+
+        while(!isGameOver())
+    }
+
+}
